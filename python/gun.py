6ed9dff4159e40f4c7a0d202c0ba9c2cfaaf75cf
Dnyarri <william.linna@gmail.com>
2013-05-14 17:47:23 +0300

    Removed useless variables from Gun-class

--- python/gun.py
+++ python/gun.py
@@ -11,21 +11,11 @@ class Gun(bge.types.KX_GameObject):
         """
         self.rate_fire = 0.3
         self.time_reload = 1.0  # How long it takes to reload
-        self.bullets = 12
-        self.clips = 4
+        self.clips = [12, 12, 12]
         self.fire_fx_size_max = 0.5
-        self.fire_fx_y = 0
-        self.fire_fx_x = 0
-        self.fire_fx_z = 0
-        # This is useful for saving bullets when reloading
-        # clip that is not empty
-        self.total_bullets_left = self.bullets * self.clips
-
 
     def update(self):
-        # FIXME: Remove this immediately when updating works
-        print("Updating gun")
-
+        pass
 
 def convert_to_gun(controller):
     old = controller.owner
