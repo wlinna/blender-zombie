109511522404f9902ffed1b65c35891519b5ff91
Dnyarri <william.linna@gmail.com>
2013-05-25 14:11:35 +0300

    Pistol's migration to gun is complete. pistol.py is no longer useful

--- python/gun.py
+++ python/gun.py
@@ -1,3 +1,22 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
 import bge
 
 class Gun(bge.types.KX_GameObject):
@@ -18,24 +37,43 @@ class Gun(bge.types.KX_GameObject):
     def message(self, controller):
         sensor = controller.sensors['message']
         subjects = sensor.subjects
+        bodies = sensor.bodies
 
         if not subjects: return
 
         for i in range(len(subjects)):
             subject = subjects[i]
-            body = subjects[i]
+            body = bodies[i]
 
             if subject == 'shoot' and body == '':
-                pass
+                self.try_to_shoot(controller)
+
+            if subject == 'reload' and body == '':
+                self.reload()
 
     def update(self):
         pass
 
     def reload(self):
-        if not self.clips:
-            print("No clips left")
+
+        # This will be used. DO NOT REMOVE
+        # if not self.clips:
+        #     print("No clips left")
+        #     return
+        # old_clip = self.clips.pop(0)
+
+        bullets = self['bullets']
+        clips = self['clips']
+        clip_size = self['clip_size']
+        delay = self['reload_delay']
+
+        if self['timer_shoot'] < self['delay']:
             return
-        old_clip = self.clips.pop(0)
+        if bullets < clip_size:
+            if clips > 0:
+                self['bullets'] = clip_size
+                self['clips'] -= 1
+                self['timer_shoot'] = 0.0
 
     def try_to_shoot(self, controller):
         if self['bullets'] <= 0:
@@ -48,13 +86,20 @@ class Gun(bge.types.KX_GameObject):
         self.shoot(controller)
 
     def shoot(self, controller):
-        if self['tap_to_shoot']:
-            pass
-
         scene = bge.logic.getCurrentScene()
         ray = controller.sensors['gun_ray']
+        # THIS WILL REPLACE bullets-property
+        # self.clips[0] -= 1
+        self['bullets'] -= 1  # This will be removed after migration is ready
         self['timer_shoot'] = 0.0
 
+        fx_object = scene.addObject("fx_gun_shot", self, 60)
+        fx_object.setParent(self, False, True)
+
+        if ray.positive:
+            target = ray.hitObject
+            if 'health' in target:
+                target['health'] -= 1
 
 
 def convert_to_gun(controller):
@@ -65,5 +110,23 @@ def convert_to_gun(controller):
     assert(old.invalid)
     assert(mutated is controller.owner)
 
+
 def update(controller):
     controller.owner.update()
+
+
+def message(controller):
+    controller.owner.message(controller)
+
+
+def ctrl_fx_shot(controller):
+    obj = controller.owner
+    scene = bge.logic.getCurrentScene()
+
+    point_gun_fire_obj = obj.parent.childrenRecursive['point_gun_fire']
+    fire_object = scene.addObject("gun_fire", "point_gun_fire", 1)
+    fire_object.setParent(point_gun_fire_obj, False, True)
+
+    act_sound = obj.actuators['sound']
+    act_sound.mode = 1
+    act_sound.startSound()
